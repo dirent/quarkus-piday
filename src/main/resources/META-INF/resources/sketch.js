92cee12db18705120d4e153dafa6f11a90d8f16a
dirk <dirk@gmx.org>
2020-03-22 19:36:14 +0100

    Implement naive version of indexOf

--- src/main/resources/META-INF/resources/sketch.js
+++ src/main/resources/META-INF/resources/sketch.js
@@ -1,15 +1,38 @@
 let searchBox, indexP, digits;
 
+function indexOf( txt, search, start ) {
+    if( search == null  ||  typeof search !== 'string' ) {
+        return -1;
+    }
+    if( txt.length < start ) {
+        return -1;
+    }
+    if( search.length == 0 ) {
+        return 0;
+    }
+    for( let i=start; i<txt.length; i++ ) {
+        for( let j=0; j<search.length; j++ ) {
+            if( search.charAt(j) !== txt.charAt(i+j) ) {
+                break;
+            }
+            if( j == search.length-1 ) {
+                return i-1;
+            }
+        }
+    }
+    return -1;
+}
+
 async function searchItUp() {
     let search = searchBox.value();
     let index;
     if (digits) {
-        index = digits.indexOf(search, 2) - 1;
+        index = indexOf(digits, search, 2);
     }
     if( index === 0 ) {
         index = 'Searching';
     }
-    if( index === -2 ) {
+    if( index === -1 ) {
         index = 'Not found';
     } 
     indexP.html(index);

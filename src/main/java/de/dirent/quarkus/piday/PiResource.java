d812784e6488c1f033238a8e82dfa058947dbb74
dirk <dirk@gmx.org>
2020-03-26 21:02:23 +0100

    Move loading pi to application start up

--- src/main/java/de/dirent/quarkus/piday/PiResource.java
+++ src/main/java/de/dirent/quarkus/piday/PiResource.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -11,15 +13,20 @@ import javax.ws.rs.core.MediaType;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
+import io.quarkus.runtime.ShutdownEvent;
+import io.quarkus.runtime.StartupEvent;
+
 @Path("/pi")
+@ApplicationScoped
 public class PiResource {
+
+    String pi = "";
 
     @ConfigProperty( name = "piday.pathtodigits" )
     String pathToDigits;
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String pi() {
+    void onStart(@Observes StartupEvent ev) {   
+        System.out.println( "Loading pi into memory..." );            
         byte[] buf = new byte[32000];
         File piDigits = new File( pathToDigits );
         try ( InputStream in = new FileInputStream( piDigits ) ) {
@@ -27,9 +34,20 @@ public class PiResource {
             while( in.read(buf) > 0 ) {
                 response.append( new String(buf, "utf-8" ) );
             }
-            return response.toString();
+            pi = response.toString();
+            System.out.println( "Loaded pi into memory." );
         } catch( Exception e) {
-            return "Could not read from digits file: " + e.getMessage();
+            System.err.println("Could not read from digits file: " + e.getMessage() );
         }
+    }
+
+    void onStop(@Observes ShutdownEvent ev) {               
+        System.out.println("The application is stopping...");
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String pi() {
+        return pi;
     }
 }

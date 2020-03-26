bf0059a989c9a827f8fb067540b428186786ff83
dirk <dirk@gmx.org>
2020-03-26 22:04:17 +0100

    Move indexOf to server side

--- src/main/java/de/dirent/quarkus/piday/PiResource.java
+++ src/main/java/de/dirent/quarkus/piday/PiResource.java
@@ -14,6 +14,7 @@ import javax.ws.rs.core.MediaType;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.jboss.resteasy.annotations.jaxrs.QueryParam;
 
 import io.quarkus.runtime.ShutdownEvent;
 import io.quarkus.runtime.StartupEvent;
@@ -34,7 +35,7 @@ public class PiResource {
         byte[] buf = new byte[32000];
         File piDigits = new File( pathToDigits );
         try ( InputStream in = new FileInputStream( piDigits ) ) {
-            StringBuffer response = new StringBuffer(1000000);
+            StringBuffer response = new StringBuffer(1000000000);
             while( in.read(buf) > 0 ) {
                 response.append( new String(buf, "utf-8" ) );
             }
@@ -49,9 +50,35 @@ public class PiResource {
         logger.debug("The application is stopping...");
     }
 
+    public int indexOf( String search, int start ) {
+        if( search == null ) {
+            return -1;
+        }
+        if( pi == null  ||  pi.length() < start ) {
+            return -1;
+        }
+        if( search.length() == 0 ) {
+            return 0;
+        }
+        for( int i=start; i<pi.length(); i++ ) {
+            for( int j=0; j<search.length(); j++ ) {
+                if( search.charAt(j) != pi.charAt(i+j) ) {
+                    break;
+                }
+                if( j == search.length()-1 ) {
+                    return i-1;
+                }
+            }
+        }
+        return -1;
+    }
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String pi() {
-        return pi;
+    public String pi( @QueryParam("search") String search ) {
+        if( search == null ) {
+            return pi.substring(0,Math.min(pi.length()-1, 1000002));
+        }
+        return ""+indexOf(search,2);
     }
 }
--- src/main/resources/META-INF/resources/sketch.js
+++ src/main/resources/META-INF/resources/sketch.js
@@ -1,52 +1,28 @@
 let searchBox, indexP, digits;
 
-function indexOf( txt, search, start ) {
-    if( search == null  ||  typeof search !== 'string' ) {
-        return -1;
-    }
-    if( txt.length < start ) {
-        return -1;
-    }
-    if( search.length == 0 ) {
-        return 0;
-    }
-    for( let i=start; i<txt.length; i++ ) {
-        for( let j=0; j<search.length; j++ ) {
-            if( search.charAt(j) !== txt.charAt(i+j) ) {
-                break;
-            }
-            if( j == search.length-1 ) {
-                return i-1;
-            }
-        }
-    }
-    return -1;
-}
-
 async function searchItUp() {
     let search = searchBox.value();
-    let index;
-    if (digits) {
-        index = indexOf(digits, search, 2);
-    }
-    if( index === 0 ) {
-        index = 'Searching';
-    }
-    if( index === -1 ) {
-        index = 'Not found';
+    if( search ) {
+        let index;
+        let response = await fetch("pi?search="+search);
+        if (response.ok) {
+            index = await response.text();
+        } else {
+            console.error("Could not search for digits in pi. HTTP-Status: " + response.status);
+        }
+        if( index === 0 ) {
+            index = 'Searching';
+        }
+        if( index === -1 ) {
+            index = 'Not found';
+        }
+        indexP.html(index);
     } 
-    indexP.html(index);
 }
 
 async function setup() {
     noCanvas();
 
-    let response = await fetch("pi");
-    if (response.ok) {
-        digits = await response.text();
-    } else {
-        console.error("Could not load digits. HTTP-Status: " + response.status);
-    }
     searchBox = createInput('');
     indexP = createP('Searching').class("result");
     searchBox.input(searchItUp);

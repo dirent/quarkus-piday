9ecedcb1f46d95b780579a80c01057c0ee3df3c5
dirk <dirk@gmx.org>
2020-12-05 10:07:45 +0100

    Delete unused tests

--- src/test/java/de/dirent/quarkus/piday/NativePiResourceIT.java
+++
@@ -1,9 +0,0 @@
-package de.dirent.quarkus.piday;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativePiResourceIT extends PiResourceTest {
-
-    // Execute the same tests but in native mode.
-}
